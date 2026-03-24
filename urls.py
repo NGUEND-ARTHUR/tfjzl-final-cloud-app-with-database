4c640f5c440c8af48cba156908dab050aac04288
Nguend Arthur Johann <154235226+NGUEND-ARTHUR@users.noreply.github.com>
2026-03-24 03:06:33 +0100

    Delete urls.py

--- urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('submit/<int:course_id>/', views.submit, name='submit'),
-    path('course/<int:course_id>/submission/<int:submission_id>/result/', views.show_exam_result, name='show_exam_result'),
-]
